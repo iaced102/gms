8147c1aa23a4916e0d50fa2191ad6d2bdc0beeff
iaced102 <lightvhp@gmail.com>
2023-09-17 23:01:06 +0700

    update

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -8,29 +8,29 @@ const messages = {
     en: enMessage,
 } as any;
 
-async function autoAssignLang() {
-    var context = import.meta.glob("../modules/*/lang/*.json");
-    var files = {} as any;
-    Object.keys(context).forEach((filePath: string) => {
-        let filePathMatch = filePath.match(
-            /\/([^/]+)\/lang\/([a-zA-Z0-9]+)\.json$/,
-        ) as string[];
-        let moduleName = filePathMatch[1] ? filePathMatch[1] : "";
-        let locale = filePathMatch[2] ? filePathMatch[2] : "";
-        if (!messages[locale]) {
-            messages[locale] = {};
-            messages[locale].module = {};
-        }
-        if (!messages[locale].module) {
-            messages[locale].module = {};
-        }
-        context[filePath]().then((a: any) => {
-            files[moduleName] = context[filePath];
-            messages[locale].module[moduleName] = a.default;
-        });
-    });
-}
-await autoAssignLang();
+// async function autoAssignLang() {
+//     var context = import.meta.glob("../modules/*/lang/*.json");
+//     var files = {} as any;
+//     Object.keys(context).forEach((filePath: string) => {
+//         let filePathMatch = filePath.match(
+//             /\/([^/]+)\/lang\/([a-zA-Z0-9]+)\.json$/,
+//         ) as string[];
+//         let moduleName = filePathMatch[1] ? filePathMatch[1] : "";
+//         let locale = filePathMatch[2] ? filePathMatch[2] : "";
+//         if (!messages[locale]) {
+//             messages[locale] = {};
+//             messages[locale].module = {};
+//         }
+//         if (!messages[locale].module) {
+//             messages[locale].module = {};
+//         }
+//         context[filePath]().then((a: any) => {
+//             files[moduleName] = context[filePath];
+//             messages[locale].module[moduleName] = a.default;
+//         });
+//     });
+// }
+// await autoAssignLang();
 
 const i18n = (locales: any) => {
     return createI18n({
