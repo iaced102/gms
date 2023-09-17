a4567f4e0612eb2b37f73c551ed0d6bf3400c120
iaced102 <lightvhp@gmail.com>
2023-09-17 23:03:46 +0700

    update

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,21 +10,21 @@ store.use(pluginPinia);
 
 export { store };
 
-export async function autoAssignStore() {
-    var context = import.meta.glob("../modules/*/store/index.ts");
-    var files = {} as any;
-    Object.keys(context).forEach((filePath: string) => {
-        let filePathMatch = filePath.match(
-            /\/([^/]+)\/store\/index.ts$/,
-        ) as string[];
-        context[filePath]().then((a: any) => {
-            const epDefault = a.default;
-            console.log(a.default);
-            if (epDefault) {
-                console.log(a.default());
-                files[filePathMatch[1]] = a.default();
-            }
-        });
-    });
-    return files;
-}
+// export async function autoAssignStore() {
+//     var context = import.meta.glob("../modules/*/store/index.ts");
+//     var files = {} as any;
+//     Object.keys(context).forEach((filePath: string) => {
+//         let filePathMatch = filePath.match(
+//             /\/([^/]+)\/store\/index.ts$/,
+//         ) as string[];
+//         context[filePath]().then((a: any) => {
+//             const epDefault = a.default;
+//             console.log(a.default);
+//             if (epDefault) {
+//                 console.log(a.default());
+//                 files[filePathMatch[1]] = a.default();
+//             }
+//         });
+//     });
+//     return files;
+// }
